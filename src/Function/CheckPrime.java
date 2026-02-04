c49e0748f1a376475f50f46c7dec68b8641cfbd3
hirotwo01 <164712153+hirotwo01@users.noreply.github.com>
2026-02-04 19:33:07 +0545

    Fix src folder structure

--- src/Function/CheckPrime.java
+++ src/Function/CheckPrime.java
@@ -3,10 +3,14 @@ package Function;
 public class CheckPrime {
     public boolean checkPrime(int num){
         boolean isPrime=true;
+        if (num <= 1) {
+            return false;
+        }
         for(int i=2;i<num;i++)
         {
             if(num%i==0){
                 isPrime=false;
+                break;
             }
         }
         return isPrime;
--- src/Function/Sum1.java
+++ src/Function/Sum1.java
@@ -1,9 +1,5 @@
 package Function;
-
-import java.util.Scanner;
-
 public class Sum1 {
-
     public int Sum (int num1, int num2, int num3){
         return num1+num2+num3;
     }

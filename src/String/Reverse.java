8e2a5a8ce1cde1d19ed4f660d922baaf6de3e744
hirotwo01 <164712153+hirotwo01@users.noreply.github.com>
2026-02-12 21:03:12 +0545

    initial commit

---
+++ src/String/Reverse.java
@@ -0,0 +1,16 @@
+package String;
+
+import java.util.Scanner;
+
+public class Reverse {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the string:");
+        String str= sc.nextLine();
+        String str1 = "";
+        for(int i=str.length()-1;i>=0;i--){
+            str1+=str.charAt(i);
+        }
+        System.out.println(str1);
+    }
+}

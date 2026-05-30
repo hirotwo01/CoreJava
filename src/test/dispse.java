c2b52d9b36f77fe855bc08802db14079f440ee4b
hirotwo01 <164712153+hirotwo01@users.noreply.github.com>
2026-05-30 11:30:05 +0545

    initial commit

--- src/test/dispse.java
+++ src/test/dispse.java
@@ -1,4 +1,12 @@
 package test;
 
+import java.util.Scanner;
+
 public class dispse {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the start and end numbers:");
+        int a= sc.nextInt();
+        int b=sc.nextInt();
+    }
 }
